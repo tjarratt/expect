39d879fec37c8902f6955beb2c413535ea7a6154
Tim Jarratt <tim.jarratt@maersk.com>
2024-10-25 20:47:02 +0200

    Empty matcher

--- lib/expect_assertions.ex
+++ lib/expect_assertions.ex
@@ -21,6 +21,33 @@ defmodule ExpectAssertions.Matchers do
     end
   end
 
+  @doc "Verifies that `expected` is an empty list, map, or tuple"
+  def to_be_empty(%{given: given} = expected) do
+    case empty?(given) do
+      :ok -> expected
+      {:error, message} -> raise matcher_error(message)
+    end
+  end
+
+  defp empty?([]), do: :ok
+  defp empty?([_ | _rest] = list), do: {:error, "Expected list '#{inspect(list)}' to be empty"}
+
+  defp empty?({}), do: :ok
+
+  defp empty?(tuple) when is_tuple(tuple) do
+    {:error, "Expected tuple '#{inspect(tuple)}' to be empty"}
+  end
+
+  defp empty?(map) when is_map(map) and map_size(map) == 0, do: :ok
+
+  defp empty?(map) when is_map(map) do
+    {:error, "Expected map '#{inspect(map)}' to be empty"}
+  end
+
+  defp empty?(otherwise) do
+    {:error, "Expected '#{inspect(otherwise)}' to be empty, but it's not a list, map, or tuple."}
+  end
+
   def to_match_regex(expected, regex) do
     if Regex.match?(regex, expected.given) do
       expected
@@ -28,6 +55,12 @@ defmodule ExpectAssertions.Matchers do
       raise ExpectAssertions.AssertionError,
         message: "Expected '#{inspect(expected.given)}' to match regex '#{inspect(regex)}'"
     end
+  end
+
+  # # # 
+
+  defp matcher_error(message) do
+    raise ExpectAssertions.AssertionError, message: message
   end
 end
 
@@ -41,5 +74,4 @@ defmodule ExpectAssertions do
   def expect(value) do
     %Expect{given: value}
   end
-
 end
--- test/expect_assertions_test.exs
+++ test/expect_assertions_test.exs
@@ -1,4 +1,5 @@
 defmodule ExpectAssertionsTest do
+  # @related [subject](lib/expect_assertions.ex)
   use ExUnit.Case
 
   alias ExpectAssertions.AssertionError
@@ -10,30 +11,54 @@ defmodule ExpectAssertionsTest do
     expect(true) |> to_equal(true)
     expect(1234) |> to_equal(1234)
     expect("ok") |> to_equal("ok")
-    expect(:ok)  |> to_equal(:ok)
+    expect(:ok) |> to_equal(:ok)
 
-    assert_raise AssertionError, "Expected 'true' to equal 'false'", fn -> 
+    assert_raise AssertionError, "Expected 'true' to equal 'false'", fn ->
       expect(true) |> to_equal(false)
     end
 
-    assert_raise AssertionError, "Expected '[1, 2, 3]' to equal '[4, 5, 6]'", fn -> 
-      expect([1,2,3]) |> to_equal([4,5,6])
+    assert_raise AssertionError, "Expected '[1, 2, 3]' to equal '[4, 5, 6]'", fn ->
+      expect([1, 2, 3]) |> to_equal([4, 5, 6])
     end
   end
 
   test "to_contain matcher" do
-    expect([1,2,3]) |> to_contain(1)
+    expect([1, 2, 3]) |> to_contain(1)
 
-    assert_raise AssertionError, "Expected '[1, 2, 3]' to contain '\"bananas\"'", fn -> 
-      expect([1,2,3]) |> to_contain("bananas")
+    assert_raise AssertionError, "Expected '[1, 2, 3]' to contain '\"bananas\"'", fn ->
+      expect([1, 2, 3]) |> to_contain("bananas")
     end
   end
 
   test "to_match_regex matcher" do
     expect("hello") |> to_match_regex(~r[^h])
 
-    assert_raise AssertionError, "Expected '\"hello world\"' to match regex '~r/NOPE/'", fn -> 
+    assert_raise AssertionError, "Expected '\"hello world\"' to match regex '~r/NOPE/'", fn ->
       expect("hello world") |> to_match_regex(~r[NOPE])
     end
+  end
+
+  test "to_be_empty matcher" do
+    expect([]) |> to_be_empty()
+    expect(%{}) |> to_be_empty()
+    expect({}) |> to_be_empty()
+
+    assert_raise AssertionError, "Expected list '[1]' to be empty", fn ->
+      expect([1]) |> to_be_empty()
+    end
+
+    assert_raise AssertionError, "Expected tuple '{1}' to be empty", fn ->
+      expect({1}) |> to_be_empty()
+    end
+
+    assert_raise AssertionError, "Expected map '%{key: \"value\"}' to be empty", fn ->
+      expect(%{key: "value"}) |> to_be_empty()
+    end
+
+    assert_raise AssertionError,
+                 "Expected '\"WHOOPS\"' to be empty, but it's not a list, map, or tuple.",
+                 fn ->
+                   expect("WHOOPS") |> to_be_empty()
+                 end
   end
 end
