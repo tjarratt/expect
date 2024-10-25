c281661e82b7578746f44036505ba9c2c49afae5
Tim Jarratt <tim.jarratt@maersk.com>
2024-10-25 21:44:11 +0200

    Add ex_docs

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Expect.MixProject do
 
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
