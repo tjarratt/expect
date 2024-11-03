7163c70af42e5c5aafe13c2a6d6dd442d3a236bd
Tim Jarratt <tim.jarratt@maersk.com>
2024-11-03 17:22:05 +0100

    Version 0.1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Expect.MixProject do
   def project do
     [
       app: :expect,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
