1ccc059e6d7c955eaf9ded9665c352fd2eba7694
Tim Jarratt <tim.jarratt@maersk.com>
2025-11-20 09:17:34 -0800

    Backfill test cases for to_not pattern_match/1

--- test/expect/matchers_test.exs
+++ test/expect/matchers_test.exs
@@ -138,10 +138,31 @@ defmodule Expect.MatchersTest do
       expect(%{year: 1999}, to: pattern_match(%Date{year: 1999, month: 12, day: 31}))
     end
 
+    test "makes variables bound in the pattern match available for later asserts" do
+      expect(%{hello: whom}, to: pattern_match(%{hello: "world"}))
+
+      expect(whom, to: equal("world"))
+    end
+
     test "shows an informative error when the pattern match fails" do
       assert_raise AssertionError,
                    ~s[Expected '%{hello: "whoopsie"}' to match pattern '%{hello: "world"}', but it did not.],
                    fn -> expect(%{hello: "whoopsie"}, to: pattern_match(%{hello: "world"})) end
+    end
+  end
+
+  describe "negative assertions with pattern_match/1" do
+    test "can pattern match maps, lists, tuples, and structs" do
+      expect(%{whoops: _oh_no}, to_not: pattern_match(%{hello: "world"}))
+      expect([_not_enough], to_not: pattern_match([1, 2, 3]))
+      expect({_not_enough}, to_not: pattern_match({:a, :b}))
+      expect(%{whoops: _my_bad}, to_not: pattern_match(%Date{year: 1999, month: 12, day: 31}))
+    end
+
+    test "shows an informative error when it does match" do
+      assert_raise AssertionError,
+                   ~s[Expected '%{hello: "world"}' to not match pattern '%{hello: "world"}', but it did.],
+                   fn -> expect(%{hello: "world"}, to_not: pattern_match(%{hello: "world"})) end
     end
   end
 end
